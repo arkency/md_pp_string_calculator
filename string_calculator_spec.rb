27fe687965bff9d5d6dff21b048c9342a7eac1ec
Paweł Pacana <pawel.pacana@gmail.com>
2014-05-23 15:19:27 +0200

    Add failing test.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,9 +1,8 @@
 require 'rspec'
-
-class StringCalculator
-
-end
+require './string_calculator'
 
 describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
 
+  specify { expect(calculator).to respond_to(:add) }
 end
