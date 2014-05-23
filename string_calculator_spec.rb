72ea1be694403301a8bde2ebcd315ff30f77a284
Marcin Domański <marcin.doman@gmail.com>
2014-05-23 15:24:11 +0200

    Added a test for the add method taking an empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,4 +5,6 @@ describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   specify { expect(calculator).to respond_to(:add) }
+
+  specify { expect(calculator.add("")).to eql(0) }
 end
