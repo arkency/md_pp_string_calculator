7277d8f7dda53386bc6314e421f825ec9dcf691a
Paweł Pacana <pawel.pacana@gmail.com>
2014-05-23 15:30:51 +0200

    Add failing spec for one number.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,7 +4,7 @@ require './string_calculator'
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
-  specify { expect(calculator).to respond_to(:add) }
-
-  specify { expect(calculator.add("")).to eql(0) }
+  specify { expect(calculator).to          respond_to(:add) }
+  specify { expect(calculator.add("")).to  eql(0) }
+  specify { expect(calculator.add("1")).to eql(1) }
 end
