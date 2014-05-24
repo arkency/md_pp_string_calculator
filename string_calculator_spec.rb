1e9715f79b1619dee7e4799bca47c6b8d6a704e9
Marcin Domański <marcin.doman@gmail.com>
2014-05-24 11:09:58 +0200

    Refactored previous tests so that we can type a bit less

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,7 +4,9 @@ require './string_calculator'
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
-  specify { expect(calculator).to          respond_to(:add) }
-  specify { expect(calculator.add("")).to  eql(0) }
-  specify { expect(calculator.add("1")).to eql(1) }
+  specify do
+    expect(calculator).to          respond_to(:add)
+    expect(calculator.add("") ).to eql(0)
+    expect(calculator.add("1")).to eql(1)
+  end
 end
