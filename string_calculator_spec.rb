c658f2a63eb2b2d86896c1a82a0cc1868face4f1
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-05 00:48:43 +0200

    Add spec for more than two arguments.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,8 +4,9 @@ require './string_calculator'
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
-  specify { expect(calculator).           to respond_to(:add) }
-  specify { expect(calculator.add("")).   to eql(0)           }
-  specify { expect(calculator.add("1")).  to eql(1)           }
-  specify { expect(calculator.add("1,2")).to eql(3)           }
+  specify { expect(calculator).           to     respond_to(:add) }
+  specify { expect(calculator.add("")).   to     eql(0)           }
+  specify { expect(calculator.add("1")).  to     eql(1)           }
+  specify { expect(calculator.add("1,2")).to     eql(3)           }
+  specify { expect(calculator.add("1,2,3,4")).to eql(10)          }
 end
