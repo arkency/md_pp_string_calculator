b96d22b65b230ae6b02cbf940257240bdd5fa5a3
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-05 00:36:46 +0200

    Back to expectation per spec style.
    Don't treat several failures as one failed spec.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,10 +4,8 @@ require './string_calculator'
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
-  specify do
-    expect(calculator).           to respond_to(:add)
-    expect(calculator.add("")).   to eql(0)
-    expect(calculator.add("1")).  to eql(1)
-    expect(calculator.add("1,2")).to eql(3)
-  end
+  specify { expect(calculator).           to respond_to(:add) }
+  specify { expect(calculator.add("")).   to eql(0)           }
+  specify { expect(calculator.add("1")).  to eql(1)           }
+  specify { expect(calculator.add("1,2")).to eql(3)           }
 end
