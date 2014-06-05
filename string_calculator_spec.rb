1d3c423cd8e1290d2985db59dc4d1b96c36b03ee
Marcin Domański <marcin.doman@gmail.com>
2014-06-05 16:36:59 +0200

    Added a more random test for adding with a custom delimiter

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -3,6 +3,7 @@ require './string_calculator'
 
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
+  CUSTOM_DELIMITERS = ((33..46).to_a + (58..63).to_a).map(&:chr)
 
   specify { expect(calculator).           to     respond_to(:add) }
   specify { expect(calculator.add("")).   to     eql(0)           }
@@ -10,5 +11,10 @@ describe StringCalculator do
   specify { expect(calculator.add("1,2")).to     eql(3)           }
   specify { expect(calculator.add("1,2,3,4")).to eql(10)          }
   specify { expect(calculator.add("1\n2\n3")).to eql(6)           }
-  specify { expect(calculator.add("//; 3;5")).to eql(8)           }
+
+  CUSTOM_DELIMITERS.sample.tap do |custom_delimiter|
+    specify "adding with a custom #{custom_delimiter} delimiter" do
+      expect(calculator.add("//#{custom_delimiter} 3#{custom_delimiter}5")).to eql(8)
+    end
+  end
 end
