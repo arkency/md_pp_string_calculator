9cbd871cb52b1b839c2c5775f21d2602673b2706
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-05 22:14:56 +0200

    Make test with custom delimiters pass.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
-  def add(numbers)
-    numbers.split(/,|\n|;/).map(&:to_i).reduce(:+).to_i
+  def add(input)
+    regex      = %r{(//(?<delimiter>.)\n)?(?<numbers>.*)}m
+    matchdata  = regex.match(input)
+    delimiters = %w(, \n) + Array(matchdata[:delimiter])
+    matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i).reduce(:+).to_i
   end
 end
