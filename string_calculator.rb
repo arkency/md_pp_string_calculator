d8214afe30787e21a03bf299b287c595bc8e489e
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-05 22:31:10 +0200

    Make specs less random - test all possible custom delimiters.

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def add(input)
     regex      = %r{(//(?<delimiter>.)\n)?(?<numbers>.*)}m
     matchdata  = regex.match(input)
-    delimiters = %w(, \n) + Array(matchdata[:delimiter])
+    delimiters = %w(, \n) + Array(matchdata[:delimiter]).map { |delimiter| Regexp.escape(delimiter) }
     matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i).reduce(:+).to_i
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -12,9 +12,7 @@ describe StringCalculator do
   specify { expect(calculator.add("1,2,3,4")).to eql(10)          }
   specify { expect(calculator.add("1\n2,3")).to  eql(6)           }
 
-  CUSTOM_DELIMITERS.sample.tap do |custom_delimiter|
-    specify "adding with a custom #{custom_delimiter} delimiter" do
-      expect(calculator.add("//#{custom_delimiter}\n3#{custom_delimiter}5")).to eql(8)
-    end
+  CUSTOM_DELIMITERS.map do |delimiter|
+    specify { expect(calculator.add("//#{delimiter}\n3#{delimiter}5")).to eql(8) }
   end
 end
