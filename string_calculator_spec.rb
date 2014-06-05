b4c169b4e4a260709f56a9f73e3cc29b8502a930
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-05 22:38:26 +0200

    Constants in rspec example groups are global for entire suite.
    At least used to be so I avoid them.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -3,7 +3,6 @@ require './string_calculator'
 
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
-  CUSTOM_DELIMITERS = ((33..46).to_a + (58..63).to_a).map(&:chr)
 
   specify { expect(calculator).           to     respond_to(:add) }
   specify { expect(calculator.add("")).   to     eql(0)           }
@@ -12,7 +11,8 @@ describe StringCalculator do
   specify { expect(calculator.add("1,2,3,4")).to eql(10)          }
   specify { expect(calculator.add("1\n2,3")).to  eql(6)           }
 
-  CUSTOM_DELIMITERS.map do |delimiter|
+  custom_delimiters = ((33..46).to_a + (58..63).to_a).map(&:chr)
+  custom_delimiters.map do |delimiter|
     specify { expect(calculator.add("//#{delimiter}\n3#{delimiter}5")).to eql(8) }
   end
 end
