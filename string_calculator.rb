6a170598828bbdb775f5cc999b3a76db91730f22
Marcin Domański <marcin.doman@gmail.com>
2014-06-10 13:55:06 +0200

    A bit of refactoring

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,12 +2,29 @@ class NegativesNotAllowed < StandardError; end
 
 class StringCalculator
   def add(input)
-    regex      = %r{(//(?<delimiter>.)\n)?(?<numbers>.*)}m
-    matchdata  = regex.match(input)
-    delimiters = %w(, \n) + Array(matchdata[:delimiter]).map { |delimiter| Regexp.escape(delimiter) }
-    numbers    = matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i)
-    negatives  = numbers.select{|n| n<0}
-    raise NegativesNotAllowed.new(numbers.join(", ")) unless negatives.empty?
+    numbers   = numbers_in(input)
+    negatives = numbers.select{|n| n<0}
+    raise NegativesNotAllowed.new(negatives.join(", ")) unless negatives.empty?
     numbers.reduce(:+).to_i
+  end
+
+  private
+
+  def regex
+    %r{(//(?<delimiter>.)\n)?(?<numbers>.*)}m
+  end
+
+  def extract_delimiters(matchdata)
+    %w(, \n) + Array(matchdata[:delimiter]).map { |delimiter| Regexp.escape(delimiter) }
+  end
+
+  def extract_numbers(matchdata, delimiters)
+    matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i)
+  end
+
+  def numbers_in(input)
+    matchdata  = regex.match(input)
+    delimiters = extract_delimiters(matchdata)
+    numbers    = extract_numbers(matchdata, delimiters)
   end
 end
