033c52b79d42c916222b2c1ec9d40042be001557
Marcin Domański <marcin.doman@gmail.com>
2014-06-10 13:41:14 +0200

    Raise the proper exception when adding negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,13 @@
+class NegativesNotAllowed < StandardError; end
+
 class StringCalculator
   def add(input)
     regex      = %r{(//(?<delimiter>.)\n)?(?<numbers>.*)}m
     matchdata  = regex.match(input)
     delimiters = %w(, \n) + Array(matchdata[:delimiter]).map { |delimiter| Regexp.escape(delimiter) }
-    matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i).reduce(:+).to_i
+    numbers    = matchdata[:numbers].split(/#{delimiters.join('|')}/).map(&:to_i)
+    negatives  = numbers.select{|n| n<0}
+    raise NegativesNotAllowed.new(numbers.join(", ")) unless negatives.empty?
+    numbers.reduce(:+).to_i
   end
 end
